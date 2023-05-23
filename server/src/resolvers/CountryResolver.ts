6335715c285bf010ea90deaa01e5afa33e1fa8c1
Aslb <anne-sophie.leber@socgen.com>
2023-05-23 17:06:25 +0200

    query by code

--- server/src/resolvers/CountryResolver.ts
+++ server/src/resolvers/CountryResolver.ts
@@ -6,7 +6,18 @@ import Country, { CountryInput } from "../entity/Country";
 export default class CountryResolver {
   @Query(() => [Country])
   async countries(): Promise<Country[]> {
-    return await db.getRepository(Country).find();
+    const countries = await db.getRepository(Country).find();
+
+    return countries.map((c) => ({ ...c, emoji: c.emoji || "" }));
+  }
+
+  @Query(() => [Country])
+  async countriesByCode(@Arg("code") code: string): Promise<Country[]> {
+    const countries = await db
+      .getRepository(Country)
+      .find({ where: { code } });
+
+    return countries.map((c) => ({ ...c, name: c.name, emoji: c.emoji || "" }));
   }
 
   @Mutation(() => Country)
